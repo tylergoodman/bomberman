03e2ea098d2aad2320792ef131deae186b6c8622
tylergoodman <finlist@gmail.com>
2014-09-10 00:46:27 -0700

    restructured server, added optional port argument, added directory listing

--- server/index.js
+++ server/index.js
@@ -1,14 +1,8 @@
 'use strict';
 
-var http = require('http'),
-	path = require('path'),
-	_ = require('lodash'),
-	// store = require('./store'),
-	// store = [],
-	peer = require('peer').PeerServer,
+var _ = require('lodash'),
 	morgan = require('morgan'),
 	body_parser = require('body-parser'),
-	serve_index = require('serve-index'),
 	express = require('express'),
 	app = express();
 
@@ -18,12 +12,6 @@ app.use(morgan('tiny'));
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({ extended: true }));
 app.disable('x-powered-by');
-
-app.use(serve_index('../www/pixi'));
-
-// static files
-app.use(express.static(path.join(__dirname, '../www')));
-app.use(express.static(path.join(__dirname, '../www')));
 
 // do stuff
 // app.post('/join', function (req, res) {
@@ -37,13 +25,4 @@ app.use(express.static(path.join(__dirname, '../www')));
 
 // });
 
-var server = http.createServer(app);
-server.listen(61337);
-
-var peer_server = new peer({port: 9000, path: '/tracker'});
-peer_server.on('connection', function (id) {
-	console.log('new peer: ' + id);
-});
-peer_server.on('disconnect', function (id) {
-	console.log('end peer: ' + id);
-});
+module.exports = app;
