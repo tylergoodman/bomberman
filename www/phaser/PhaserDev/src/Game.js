2d74d0acd36ec89a403708d40f17b7589ba19ad5
Steven <lam.steven94@yahoo.com>
2014-09-27 02:46:20 -0700

    Added a background layer

--- www/phaser/PhaserDev/src/Game.js
+++ www/phaser/PhaserDev/src/Game.js
@@ -3,20 +3,21 @@ function Game ()
 {
 	var world = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update })
 	var player = null;
+
+	// Preload images needed
 	function preload() {
 
-	    //  You can fill the preloader with as many assets as your game requires
-
-	    //  Here we are loading an image. The first parameter is the unique
-	    //  string by which we'll identify the image later in our code.
-
-	    //  The second parameter is the URL of the image (relative)
 	    world.load.image('bomberman', 'assets/bomberman.jpg')
+	    world.load.image('background', 'assets/background.png')
 
 	}
 
 	function create() {
-
+		// background
+		var background = world.add.group();
+   		background.z = 1;
+   		background.add(world.add.sprite(0,0,'background'))
+		// player
 		player = new Player(world, "Player 1", 0, 0, 0, 0)
 
 	}
