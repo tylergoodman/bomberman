bd37cdd93127fd79a4f38b75d852a702e703dd4a
Steven L <lam.steven94@yahoo.com>
2014-10-28 17:18:18 -0700

    Added game to UI and game now scales

--- www/phaser/PhaserDev/src/Preloader.js
+++ www/phaser/PhaserDev/src/Preloader.js
@@ -2,10 +2,19 @@ var Preloader = function(game) {}
 
 Preloader.prototype = {
   preload: function() { this.load.image('background', 'assets/titlescreen.jpg')},
-  create:  function() {	// background
+  create:  function() {	
+						//  This sets a limit on the up-scale
+					    //this.game.scale.maxWidth = 800;
+					    //this.game.scale.maxHeight = 600;
+
+					    //  Then we tell Phaser that we want it to scale up to whatever the browser can handle, but to do it proportionally
+					    this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+					    this.game.scale.setScreenSize();
+  						// background
 						var background = this.game.add.group();
 				   		background.z = 1;
-				   		background.add(this.game.add.sprite(0,0,'background'))},
+				   		background.add(this.game.add.sprite(0,0,'background'))
+				   	  },
   update:  function() {
 
 		if(this.game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR, 10))
