940efd7ead22cf7716dd2abe10cdfef1751daa5f
Steven <lam.steven94@yahoo.com>
2014-11-09 19:07:22 -0800

    got preloader to resize initially

--- www/phaser/PhaserDev/src/Preferences.js
+++ www/phaser/PhaserDev/src/Preferences.js
@@ -3,7 +3,6 @@ function Preferences(world, players)
 	this.World = world
 	this.BoardColSize = 15
 	this.BoardRowSize =  9
-	this.Div = document.getElementById('game')
 	this.ImageSize = 70
 	this.Players = players
 }
--- www/phaser/PhaserDev/src/Preloader.js
+++ www/phaser/PhaserDev/src/Preloader.js
@@ -1,3 +1,9 @@
+var bg = null
+var image = null
+var scaleWidth = null
+var scaleHeight = null
+var ratio = 1;
+
 var Preloader = function(game) {} 
 
 Preloader.prototype = {
@@ -8,14 +14,51 @@ Preloader.prototype = {
 					    //this.game.scale.maxHeight = 600;
 
 					    //  Then we tell Phaser that we want it to scale up to whatever the browser can handle, but to do it proportionally
-					    this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-					    this.game.scale.setScreenSize();
+					    this.game.scale.maxWidth = document.getElementById('game').offsetWidth;
+    					this.game.scale.maxHeight = document.getElementById('game').offsetHeight;
+					    this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL
+					    this.game.scale.setScreenSize()
+
   						// background
 						var background = this.game.add.group();
 				   		background.z = 1;
-				   		background.add(this.game.add.sprite(0,0,'background'))
+				   		bg = this.game.add.sprite(0,0,'background')
+				   		image = game.cache.getImage('background')
+
+				   		scaleWidth = document.getElementById('game').offsetWidth / image.width 
+				   	    scaleHeight = document.getElementById('game').offsetHeight / image.height 
+
+				   		background.add(bg)
+				   		
+				   	    if(scaleWidth > scaleHeight)
+				   	    {
+				   	    	ratio = scaleWidth;
+				   			bg.scale.setTo(ratio, ratio);
+				   	    }
+				   	    else 
+				   	    {
+				   	    	ratio = scaleHeight;
+				   			bg.scale.setTo(ratio, ratio);
+				   	    }
+
 				   	  },
   update:  function() {
+
+   		image = game.cache.getImage('background')
+
+   		scaleWidth = document.getElementById('game').offsetWidth / image.width 
+   	    scaleHeight = document.getElementById('game').offsetHeight / image.height 
+
+/*
+   	    console.log( "window: " + document.getElementById('game').offsetWidth )
+   	    console.log( "window: " + document.getElementById('game').offsetHeight )
+   	    console.log( "image: " +bg.width )
+
+   	    console.log( "image: " + bg.height )
+*/
+   			
+   		bg.scale.setTo(scaleWidth, scaleHeight);
+
 
 		if(this.game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR, 10))
 			this.game.state.start('Game');
