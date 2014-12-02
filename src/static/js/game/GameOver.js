e0eac7579fb621d8799f6833d8565bc5eee751b1
Steven <lam.steven94@yahoo.com>
2014-12-01 22:47:53 -0800

    Added Credits music

--- src/static/js/game/GameOver.js
+++ src/static/js/game/GameOver.js
@@ -1,7 +1,10 @@
 var GameOver = function(game) {} 
 
 GameOver.prototype = {
-  preload: function() { this.load.image('gameover', './static/img/gameover.png')},
+  preload: function() { 
+  						this.load.image('gameover', './static/img/gameover.png')
+  						game.load.audio('credits', ['./static/audio/credits.mp3', './static/audio/credits.mp3']);
+					  },
   create:  function() {	
   						// background
 						var background = this.game.add.group();
@@ -15,10 +18,21 @@ GameOver.prototype = {
 				   	    var scaleHeight = document.getElementById('game').offsetHeight / image.height 
 
 				   		gameOverSprite.scale.setTo(scaleWidth, scaleHeight)
+
+				   		// Play Credits music
+						this.creditsMusic = game.add.audio('credits', 1, true);
+			   			this.creditsMusic.play();
+
 				   	  },
   update:  function() {
 
 		if(this.game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR, 10))	
 			this.game.state.start('Preloader');
+  },
+
+  shutdown: function() {
+  		
+  		// stop playing music
+  		this.creditsMusic.stop();
   }
 }
--- www/static/js/game.built.js
+++ www/static/js/game.built.js
@@ -1783,7 +1783,10 @@ GameState.prototype = {
 var GameOver = function(game) {} 
 
 GameOver.prototype = {
-  preload: function() { this.load.image('gameover', './static/img/gameover.png')},
+  preload: function() { 
+  						this.load.image('gameover', './static/img/gameover.png')
+  						game.load.audio('credits', ['./static/audio/credits.mp3', './static/audio/credits.mp3']);
+					  },
   create:  function() {	
   						// background
 						var background = this.game.add.group();
@@ -1797,11 +1800,22 @@ GameOver.prototype = {
 				   	    var scaleHeight = document.getElementById('game').offsetHeight / image.height 
 
 				   		gameOverSprite.scale.setTo(scaleWidth, scaleHeight)
+
+				   		// Play Credits music
+						this.creditsMusic = game.add.audio('credits', 1, true);
+			   			this.creditsMusic.play();
+
 				   	  },
   update:  function() {
 
 		if(this.game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR, 10))	
 			this.game.state.start('Preloader');
+  },
+
+  shutdown: function() {
+  		
+  		// stop playing music
+  		this.creditsMusic.stop();
   }
 }
 var game = new Phaser.Game("100", "100", Phaser.AUTO, 'game')
